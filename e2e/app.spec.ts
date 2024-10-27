457e389012326f5d24cbbed650b1ead7a79f66df
Paul Csapak <hello@paulcpk.com>
2024-10-27 20:54:37 +0100

    upgrade packages

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,12 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test("should navigate to the about page", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("/");
-  // Find an element with the text 'About' and click on it
-  await page.click("text=About");
-  // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("/about");
-  // The new page should contain an h1 with "About"
-  await expect(page.locator("h1")).toContainText("About");
+  // Go to home page - should contain two buttons
+  await expect(page.locator("main")).toContainText("Deploy now");
+  await expect(page.locator("main")).toContainText("Read our docs");
 });
